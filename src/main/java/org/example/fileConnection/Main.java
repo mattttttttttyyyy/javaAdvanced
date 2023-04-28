92af1b5bada91e19b0a6f18791a975712e30064a
mattttttttttyyyy <130585990+mattttttttttyyyy@users.noreply.github.com>
2023-04-28 15:12:56 +0200

    Exceptions books

--- src/main/java/org/example/fileConnection/Main.java
+++ src/main/java/org/example/fileConnection/Main.java
@@ -17,8 +17,6 @@ package org.example.fileConnection;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
