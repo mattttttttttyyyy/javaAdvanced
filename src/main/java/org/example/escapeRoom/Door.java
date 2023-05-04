ea3a5ae0cff074a70fce358f97ee0ee2ecc0086a
mattttttttttyyyy <130585990+mattttttttttyyyy@users.noreply.github.com>
2023-05-04 09:55:12 +0200

    escape room update

--- src/main/java/org/example/escapeRoom/Door.java
+++ src/main/java/org/example/escapeRoom/Door.java
@@ -1,20 +1,8 @@
 package org.example.escapeRoom;
 
 public class Door extends allElements {
-    boolean doorOpen = false;
     public Door(){
         this.name = "door";
 
-    }
-    public Door(boolean doorOpen) {
-        this.doorOpen = doorOpen;
-    }
-
-    public boolean isDoorOpen() {
-        return doorOpen;
-    }
-
-    public void setDoorOpen(boolean doorOpen) {
-        this.doorOpen = doorOpen;
     }
 }
